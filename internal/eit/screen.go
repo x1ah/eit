7ef25886bec678b43227d7e9fb552a15a2f73ec3
x1ah <x1ahgxq@gmail.com>
2019-10-01 16:47:16 +0800

    init screen

--- internal/eit/screen.go
+++ internal/eit/screen.go
@@ -1,1 +1,27 @@
 package eit
+
+import (
+	"fmt"
+	"github.com/gdamore/tcell"
+	"os"
+)
+
+func InitScreen() tcell.Screen {
+	tcell.SetEncodingFallback(tcell.EncodingFallbackUTF8)
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	s.SetStyle(tcell.StyleDefault)
+
+	s.Clear()
+
+	return s
+}
